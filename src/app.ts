fa8c1f5eab1d4357ef8869e1f199b2de467bed62
lebrancconvas <lebrancconvas@gmail.com>
2022-04-16 19:11:45 +0700

    ✨ Re-Draft Class.

--- src/app.ts
+++ src/app.ts
@@ -1,104 +1,59 @@
-class Character 
-{
-	name: string;
-
-	constructor(name: string) 
-	{
-		this.name = name;
-	}
-}
-
-class PlayableCharacter extends Character 
-{
-	hp: number;
-	atk: number;
-	def: number;
-
-	constructor(name: string, hp: number, atk: number, def: number) 
-	{
-		super(name);
-		this.hp = hp;
-		this.atk = atk;
-		this.def = def;
-	}
-
-	showStatus()
-	{
-		console.log(`-- ${this.name} --\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
-	}
-
-	attack()
-	{
-		
-	}
-}
-
-class NonPlayableCharacter extends Character 
+class Character
 {
 
 }
 
-class EnemyCharacter extends Character 
+class CombatableCharacter extends Character
 {
 
 }
 
-class Warrior extends PlayableCharacter 
-{
-	constructor(name: string, hp: number, atk: number, def: number) 
-	{
-		super(name, hp, atk, def);
-	}
-
-	showStatus()
-	{
-		console.log(`-- ${this.name} --\nClass: Warrior\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
-	}
-}
-
-class Mage extends PlayableCharacter 
-{
-	constructor(name: string, hp: number, atk: number, def: number) 
-	{
-		super(name, hp, atk, def);
-	}
-
-	showStatus()
-	{
-		console.log(`-- ${this.name} --\nClass: Mage\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
-	}
-}
-
-class Paladin extends PlayableCharacter 
-{
-	constructor(name: string, hp: number, atk: number, def: number) 
-	{
-		super(name, hp, atk, def);
-	}
-
-	showStatus()
-	{
-		console.log(`-- ${this.name} --\nClass: Paladin\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
-	}
-}
-
-class BossEnemy extends EnemyCharacter 
+class NonPlayableCharacter extends Character
 {
 
 }
 
-class Villager extends NonPlayableCharacter 
+class PlayableCharacter extends CombatableCharacter
 {
 
 }
 
-class Merchant extends NonPlayableCharacter 
+class EnemyCharacter extends CombatableCharacter
 {
 
 }
 
-const vivi = new Mage("Vivi", 100, 12, 14);
-const cecil = new Paladin("Cecil", 120, 20, 16);
+class Warrior extends PlayableCharacter
+{
+	
+}
+class Mage extends PlayableCharacter
+{
+	
+}
+class Archer extends PlayableCharacter
+{
+	
+}
+class Paladin extends PlayableCharacter
+{
+	
+}
+class Dragoon extends PlayableCharacter
+{
+	
+}
+class Theft extends PlayableCharacter
+{
+	
+}
 
-vivi.showStatus();
-cecil.showStatus();
+class Villager extends NonPlayableCharacter
+{
+
+}
+
+class Merchant extends NonPlayableCharacter
+{
+	
+}
