9f6b9963ec1565d37accad351516d3764d9608af
lebrancconvas <lebrancconvas@gmail.com>
2022-04-16 19:24:49 +0700

    ✨ Inheritence, Method, and Instance.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,39 @@
 class Character
 {
-
+	name: string;
+	
+	constructor(name: string)
+	{
+		this.name = name;
+	}
 }
 
 class CombatableCharacter extends Character
 {
+	name!: string;
+	hp: number;
+	atk: number;
+	def: number;
 
+	constructor(name: string, hp: number, atk: number, def: number)
+	{
+		super(name);
+		this.hp = hp;
+		this.atk = atk;
+		this.def = def;
+	}
+
+	showStatus()
+	{
+		console.log(`-- ${this.name} --\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
+	}
+
+	attack(target: CombatableCharacter)
+	{
+		const damage: number = this.atk - (target.def * 0.2);
+		target.hp -= damage;
+		console.log(`${this.name} attacks ${target.name}\n${target.name} is received ${damage} damages.`);
+	}
 }
 
 class NonPlayableCharacter extends Character
@@ -62,3 +90,17 @@ class Merchant extends NonPlayableCharacter
 {
 
 }
+
+
+const lucia = new Warrior("Lucia XII", 200, 27, 20);
+const mafia = new EnemyCharacter("Mafia", 120, 30, 10);
+
+lucia.showStatus();
+mafia.showStatus();
+
+lucia.attack(mafia);
+mafia.attack(lucia);
+
+lucia.showStatus();
+mafia.showStatus();
+
