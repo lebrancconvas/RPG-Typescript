ce35e3a97e199a335570607160a603de40602ad5
lebrancconvas <lebrancconvas@gmail.com>
2022-04-16 18:47:15 +0700

    ✨ Inheritance, Instance, and Method.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,36 @@
 class Character 
 {
+	name: string;
 
+	constructor(name: string) 
+	{
+		this.name = name;
+	}
 }
 
 class PlayableCharacter extends Character 
 {
+	hp: number;
+	atk: number;
+	def: number;
 
+	constructor(name: string, hp: number, atk: number, def: number) 
+	{
+		super(name);
+		this.hp = hp;
+		this.atk = atk;
+		this.def = def;
+	}
+
+	showStatus()
+	{
+		console.log(`-- ${this.name} --\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
+	}
+
+	attack()
+	{
+		
+	}
 }
 
 class NonPlayableCharacter extends Character 
@@ -28,6 +53,11 @@ class Mage extends PlayableCharacter
 
 }
 
+class Paladin extends PlayableCharacter 
+{
+
+}
+
 class BossEnemy extends EnemyCharacter 
 {
 
@@ -42,3 +72,9 @@ class Merchant extends NonPlayableCharacter
 {
 
 }
+
+const vivi = new Mage("Vivi", 100, 12, 14);
+const cecil = new Paladin("Cecil", 120, 20, 16);
+
+vivi.showStatus();
+cecil.showStatus();
