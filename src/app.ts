97719d5aab0f7a56b6174802bc943f670f8b3a4f
lebrancconvas <lebrancconvas@gmail.com>
2022-04-16 18:35:20 +0700

    🔨 Draft Classes.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,44 @@
-// Code Something. 
+class Character 
+{
+
+}
+
+class PlayableCharacter extends Character 
+{
+
+}
+
+class NonPlayableCharacter extends Character 
+{
+
+}
+
+class EnemyCharacter extends Character 
+{
+
+}
+
+class Warrior extends PlayableCharacter 
+{
+
+}
+
+class Mage extends PlayableCharacter 
+{
+
+}
+
+class BossEnemy extends EnemyCharacter 
+{
+
+}
+
+class Villager extends NonPlayableCharacter 
+{
+
+}
+
+class Merchant extends NonPlayableCharacter 
+{
+
+}
