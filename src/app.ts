6fbac07c42f6886c3754bd4a01cc39df497b2f5a
lebrancconvas <lebrancconvas@gmail.com>
2022-04-16 21:16:25 +0700

    ✨ Update Code.

--- src/app.ts
+++ src/app.ts
@@ -28,6 +28,13 @@ class CombatableCharacter extends Character
 		console.log(`-- ${this.name} --\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
 	}
 
+	summaryStatus(target: CombatableCharacter)
+	{
+		console.log("SUMMARY");
+		this.showStatus();
+		target.showStatus();
+	}
+
 	attack(target: CombatableCharacter)
 	{
 		// Damage Calculation. 
@@ -48,6 +55,8 @@ class CombatableCharacter extends Character
 		{
 			console.log(`${this.name} cannot attack, because ${this.name} is fainted.`);
 		}
+
+		this.summaryStatus(target);
 	}
 
 	checkFaint()
@@ -78,42 +87,102 @@ class NonPlayableCharacter extends Character
 
 class PlayableCharacter extends CombatableCharacter
 {
+	useItem(target: CombatableCharacter)
+	{
 
+	}
 }
 
 class EnemyCharacter extends CombatableCharacter
 {
+	heldItems: string[];
 
+	constructor(name: string, hp: number, atk: number, def: number, heldItems: string[])
+	{
+		super(name, hp, atk, def);
+		this.heldItems = heldItems;
+	}
 }
 
 class Warrior extends PlayableCharacter
 {
-	
+	showStatus()
+	{
+		console.log(`-- ${this.name} --\nClass: Warrior\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
+	}
 }
 
 class Mage extends PlayableCharacter
 {
-	
-}
+	mp: number;
 
-class Archer extends PlayableCharacter
-{
-	
+	constructor(name: string, hp: number, mp: number, atk: number, def: number)
+	{
+		super(name, hp, atk, def);
+		this.mp = mp;
+	}
+
+	showStatus()
+	{
+		console.log(`-- ${this.name} --\nClass: Mage\nHP: ${this.hp}\nMP: ${this.mp}\nATK: ${this.atk}\nDEF: ${this.def}`);
+	}
+
+	useSkill(skill: Skill, target: CombatableCharacter)
+	{
+		this.mp -= skill.mana;
+		target.hp -= skill.effect;
+		console.log(`${this.name} uses ${skill.name} to ${target.name}\n${target.name} is received ${skill.effect} damages.`);
+		this.summaryStatus(target);
+	}	
 }
 
 class Paladin extends PlayableCharacter
 {
-	
+	mp: number;
+	constructor(name: string, hp: number, mp: number, atk: number, def: number)
+	{
+		super(name, hp, atk, def);
+		this.mp = mp;
+	}
+
+	showStatus()
+	{
+		console.log(`-- ${this.name} --\nClass: Paladin\nHP: ${this.hp}\nMP: ${this.mp}\nATK: ${this.atk}\nDEF: ${this.def}`);
+	}
+
+	useSkill(skill: Skill, target: CombatableCharacter)
+	{
+		this.mp -= skill.mana;
+		target.hp += skill.effect;
+		console.log(`${this.name} uses ${skill.name} to ${target.name}\n${target.name} is received ${skill.effect} damages.`);
+		this.summaryStatus(target); 
+	}	
 }
 
 class Dragoon extends PlayableCharacter
 {
-	
+	showStatus()
+	{
+		console.log(`-- ${this.name} --\nClass: Dragoon\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
+	}
+
+	jump()
+	{
+
+	}
 }
 
 class Theft extends PlayableCharacter
 {
-	
+	showStatus()
+	{
+		console.log(`-- ${this.name} --\nClass: Theft\nHP: ${this.hp}\nATK: ${this.atk}\nDEF: ${this.def}`);
+	}
+
+	steal(target: EnemyCharacter)
+	{
+
+	}
 }
 
 class Villager extends NonPlayableCharacter
@@ -123,13 +192,55 @@ class Villager extends NonPlayableCharacter
 
 class Merchant extends NonPlayableCharacter
 {
+	trade(MerchantItem: Item, PlayerItem: Item)
+	{
 
+	}
 }
 
+class CombatStuff
+{
+	name: string;
+	description: string;
+	effect: number;
+	support: boolean;
+
+	constructor(name: string, description: string, effect: number, support: boolean)
+	{
+		this.name = name;
+		this.description = description;
+		this.effect = effect;
+		this.support = support;
+	}
+}
+
+class Skill extends CombatStuff
+{
+	mana: number;
+
+	constructor(name: string, description: string, mana: number, effect: number, support: boolean)
+	{
+		super(name, description, effect, support);
+		this.mana = mana;
+	}
+}
+
+class Item extends CombatStuff
+{
+	amount: number;
+
+	constructor(name: string, description: string, amount: number, effect: number, support: boolean)
+	{
+		super(name, description, effect, support);
+		this.amount = amount;
+	}
+}
+
+// Implementation. 
 const combatTest = () => 
 {
 	const lucia = new Warrior("Lucia XII", 200, 27, 20);
-	const mafia = new EnemyCharacter("Mafia", 120, 30, 10);
+	const mafia = new EnemyCharacter("Mafia II", 120, 30, 10, ["Golden Ticket"]);
 	
 	lucia.showStatus();
 	mafia.showStatus();
@@ -152,8 +263,17 @@ const npcTest = () =>
 	villagerA.greet();
 }
 
-combatTest();
-npcTest();
+const skillTest = () => 
+{
+	const vivi = new Mage("Vivi", 100, 120, 20, 10);
+	const eveman = new EnemyCharacter("Eve Man", 200, 50, 20, []);
+	const fire = new Skill("Fire", "Using fire to burn the enemies", 3, 5, false);
 
+	vivi.useSkill(fire, eveman);
+}
+
+// combatTest();
+// npcTest();
+skillTest();
 
 
